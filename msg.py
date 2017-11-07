078802de3d84a05b860bd734303d6375d540e24e
Borna Bešić <besic.borna@gmail.com>
2017-11-07 14:43:49 +0100

    Add terminal colors for log messages

--- msg.py
+++ msg.py
@@ -1,9 +1,15 @@
 
+class TermColor:
+    INFO = "\033[94m" # blue
+    WARNING = "\033[93m" # yellow
+    ERROR = "\033[91m" # red
+    END = "\033[0m"
+
 def info(text):
-	print("[INFO] {}".format(text))
+	print("{}[INFO]{} {}".format(TermColor.INFO, TermColor.END, text))
 
 def warning(text):
-	print("[WARNING] {}".format(text))
+	print("{}[WARNING]{} {}".format(TermColor.WARNING, TermColor.END, text))
 
 def error(text):
-	print("[ERROR] {}".format(text))
+	print("{}[ERROR]{} {}".format(TermColor.ERROR, TermColor.END, text))
