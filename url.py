efc0ddce8c61e525e87c5c510e5e22619ff0d8c5
Borna Bešić <besic.borna@gmail.com>
2018-12-24 11:52:19 +0100

    Remove unused functions

--- url.py
+++ url.py
@@ -1,6 +1,5 @@
 import re
 from urllib.request import urlopen
-import urllib.error
 
 def get(url, timeout = 10):
     try:
@@ -10,20 +9,11 @@ def get(url, timeout = 10):
     except:
         return None
 
-def ensure_relative_path(link, base):
-    return link.replace(base, "")
-
-def regex_filter(regex, links):
-    pattern = re.compile(regex)
-    filtered = set()
-    for link in links:
-        if pattern.match(link)==None:
-            continue
-        filtered.add(link)
-
-    return filtered
-
 _URL_PATTERN = re.compile("href=\"([^\"]*)\"")
+def links_from_html(html):
+    links = _URL_PATTERN.findall(html)
+    return set(links)
+
 def content_and_links(url):
     html = get(url)
     if html is None:
@@ -31,21 +21,3 @@ def content_and_links(url):
     else:
         links = links_from_html(html)
     return html, links
-
-def links_from_html(html):
-    links = _URL_PATTERN.findall(html)
-    return set(links)
-
-def filter_valid_links(links, categories, base_url):
-    relative_links = set(map(lambda link: ensure_relative_path(link, base_url), links))
-    filtered_links = set()
-
-    for category in categories:
-        rtype = type(category["regex"])
-        if rtype is str: # single regex
-            filtered_links.update(regex_filter(category["regex"], relative_links))
-        elif rtype is list: # multiple regexes
-            for regex in category["regex"]:
-                filtered_links.update(regex_filter(regex, relative_links))
-
-    return filtered_links
